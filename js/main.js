6e270791997e01dfd090e389179d8711fa9a69f0
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2022-12-10 17:04:33 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// TODO: (if needed) add your javascript code here :)
+
+// console.info(
+//         "%cStyling logs with css is fun :)",
+//         "color:green;font-family:system-ui;font-size: 2rem;border: 2px solid red;padding: 1rem; margin: 5px;"
+//       );
